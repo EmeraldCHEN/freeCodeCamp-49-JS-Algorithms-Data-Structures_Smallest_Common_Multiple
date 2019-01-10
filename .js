60b20fed74042a688e9eb3b8dff346afc17141b3
Emerald Chen <ahyuancy@gmail.com>
2019-01-11 12:33:48 +1300

    Update .js

--- .js
+++ .js
@@ -1,5 +1,6 @@
 function smallestCommons(arr) {
-  // Sort array from greater to lowest
+  // Because of the possibility of the smallest common denominator being among the two biggest numbers, 
+  // it makes sense to check those first, so sort the array from greater to lowest
   arr.sort((a, b) => b - a);
 
   // Create a new array and add all values from the sorted array in descending order 
@@ -21,9 +22,9 @@ function smallestCommons(arr) {
     loop++;
    // console.log(loop);
    // console.log(n);
-  } while (n !== newArr.length);
+  } while (n !== newArr.length); // When n === newArr.length, quotient % every single number in newArr === 0
 
   return quotient;
 }
-let num = smallestCommons([1,6]);
-console.log(num);
+smallestCommons([1,6]); // 60
+
